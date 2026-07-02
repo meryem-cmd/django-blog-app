bfd02d3aaf38294dc52f33729e219103239b6d14
meryem-cmd <tanvirmeryem2@gmail.com>
2026-07-02 16:47:51 +0500

    Configure Django admin for Blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Blog
+#the blog model that is created is imported here
 
 # Register your models here.
+
+@admin.register(Blog) 
+# show the Blog model in admin panel
+class BlogAdmin(admin.ModelAdmin):
+  list_display = ("title", "is_published", "created_at")
+  list_filter = ("is_published", "created_at")
+  search_fields = ("title", "content")
