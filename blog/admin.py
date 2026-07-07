c1878bdf3a3ba1072f282d8322c7a752ceae80b0
Maryyam <tanvirmeryem2@gmail.com>
2026-07-07 15:47:56 +0500

    Split admin registration between blog and accounts

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from .models import Blog, Follow
-#the blog model that is created is imported here
+from .models import Blog
 
-# Register your models here.
-
-@admin.register(Blog) 
-# show the Blog model in admin panel
+@admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-  list_display = ("title", "author", "is_published", "visibility", "created_at")
-  list_filter = ("is_published", "visibility", "created_at")
-  search_fields = ("title", "content")
-
-
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-  list_display = ("follower", "following", "created_at")
-  search_fields = ("follower__username", "following__username")
+    list_display = ("title", "author", "is_published", "visibility", "created_at")
+    list_filter = ("is_published", "visibility", "created_at")
+    search_fields = ("title", "content")
